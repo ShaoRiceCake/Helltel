1ed2f0e66971e9f0935d89264fe05940a57874f7
Oliver Craft <128704494+ShaoRiceCake@users.noreply.github.com>
2025-03-14 18:23:48 +0800

    修改编码格式为utf8

--- Assets/Scripts/AI/Pathlist.cs
+++ Assets/Scripts/AI/Pathlist.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 public class Pathlist: MonoBehaviour
 {
 
-    public List<Transform> waypoints;//Ñ°Â·Â·¾¶µã
+    public List<Transform> waypoints;//å¯»è·¯è·¯å¾„ç‚¹
     [SerializeField]
     private bool alwaysDrawPath;
     [SerializeField]
@@ -18,7 +18,7 @@ public class Pathlist: MonoBehaviour
     {
         if (alwaysDrawPath)
         {
-            DrawPath();//»æÖÆÂ·¾¶
+            DrawPath();//ç»˜åˆ¶è·¯å¾„
         }
     }
     public void DrawPath()
