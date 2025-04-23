87dc5cfd9632a2b94eb845672a87843936e45339
Tiancheng Wang <53307458+thefamer@users.noreply.github.com>
2025-04-24 03:59:12 +0800

    修正外轮廓控制
    修正为使用object layer控制，已默认设置为User Layer 11（名为Item），如需更改，请进入rendering/Helltel Universal Render Pipeline Asset_Renderer中的renderer features，找到最下面的Outline Feature，修改Layer Mask即可

--- Assets/Rendering/Render Feats/Outline.cs
+++ Assets/Rendering/Render Feats/Outline.cs
@@ -5,6 +5,35 @@ using UnityEngine.Rendering.Universal;
 
 public class ItemOutlineFeature : ScriptableRendererFeature
 {
+    [SerializeField, Tooltip("Layer mask for objects to outline (User Layer 11 = 'Item')")]
+    private LayerMask m_LayerMask = 1 << 11;
+
+    [SerializeField] private Material m_OutlineMaterial;
+    private OutlineRenderPass m_OutlineRenderPass;
+
+    private bool IsMaterialValid => m_OutlineMaterial && m_OutlineMaterial.shader && m_OutlineMaterial.shader.isSupported;
+
+    public override void Create()
+    {
+        if (!IsMaterialValid)
+            return;
+
+        m_OutlineRenderPass = new OutlineRenderPass(m_OutlineMaterial, m_LayerMask);
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (m_OutlineRenderPass == null)
+            return;
+
+        renderer.EnqueuePass(m_OutlineRenderPass);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        m_OutlineRenderPass?.Dispose();
+    }
+
     class OutlineRenderPass : ScriptableRenderPass
     {
         private static readonly List<ShaderTagId> s_shaderTagIds = new List<ShaderTagId>(){
@@ -12,24 +41,26 @@ public class ItemOutlineFeature : ScriptableRendererFeature
             new ShaderTagId("UniversalForward"),
             new ShaderTagId("UniversalForwardOnly"),
         };
+
         private static readonly int s_ShaderProp_OutlineMask = Shader.PropertyToID("_OutlineMask");
+
         private readonly Material m_OutlineMaterial;
         private readonly FilteringSettings m_FilteringSettings;
         private readonly MaterialPropertyBlock m_PropertyBlock;
         private RTHandle m_OutlineMaskRT;
 
-        public OutlineRenderPass(Material outlineMaterial){
-
+        public OutlineRenderPass(Material outlineMaterial, LayerMask layerMask)
+        {
             renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
 
             m_OutlineMaterial = outlineMaterial;
-            //第二层layer，00.....010 = 2
-            m_FilteringSettings = new FilteringSettings(RenderQueueRange.all, renderingLayerMask:2);
+            // 改用物体图层
+            m_FilteringSettings = new FilteringSettings(RenderQueueRange.all, layerMask);
             m_PropertyBlock = new MaterialPropertyBlock();
-
         }
 
-        public void Dispose(){
+        public void Dispose()
+        {
             m_OutlineMaskRT?.Release();
             m_OutlineMaskRT = null;
         }
@@ -41,61 +72,28 @@ public class ItemOutlineFeature : ScriptableRendererFeature
             desc.msaaSamples = 1;
             desc.depthBufferBits = 0;
             desc.colorFormat = RenderTextureFormat.ARGB32;
-            RenderingUtils.ReAllocateIfNeeded(ref m_OutlineMaskRT, desc,name:"_OutlineMaskRT");
+            RenderingUtils.ReAllocateIfNeeded(ref m_OutlineMaskRT, desc, name: "_OutlineMaskRT");
         }
-
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             var cmd = CommandBufferPool.Get("Outline Command");
 
             cmd.SetRenderTarget(m_OutlineMaskRT);
-            cmd.ClearRenderTarget(true,true,Color.clear);
-            var drawingSettings = CreateDrawingSettings(s_shaderTagIds,ref renderingData,SortingCriteria.None);
-            var rendererListParams = new RendererListParams(renderingData.cullResults,drawingSettings,m_FilteringSettings);
+            cmd.ClearRenderTarget(true, true, Color.clear);
+
+            var drawingSettings = CreateDrawingSettings(s_shaderTagIds, ref renderingData, SortingCriteria.None);
+            var rendererListParams = new RendererListParams(renderingData.cullResults, drawingSettings, m_FilteringSettings);
             var list = context.CreateRendererList(ref rendererListParams);
             cmd.DrawRendererList(list);
 
             cmd.SetRenderTarget(renderingData.cameraData.renderer.cameraColorTargetHandle);
             m_PropertyBlock.SetTexture(s_ShaderProp_OutlineMask, m_OutlineMaskRT);
-            cmd.DrawProcedural(Matrix4x4.identity,m_OutlineMaterial,0,MeshTopology.Triangles,3,1,m_PropertyBlock);
+            cmd.DrawProcedural(Matrix4x4.identity, m_OutlineMaterial, 0, MeshTopology.Triangles, 3, 1, m_PropertyBlock);
 
             context.ExecuteCommandBuffer(cmd);
             cmd.Clear();
             CommandBufferPool.Release(cmd);
-
         }
-
-    }
-
-    [SerializeField] private Material m_OutlineMaterial;
-    OutlineRenderPass m_OutlineRenderPass;
-
-    private bool IsMaterialValid => m_OutlineMaterial && m_OutlineMaterial.shader && m_OutlineMaterial.shader.isSupported;
-    /// <inheritdoc/>
-    public override void Create()
-    {
-        if(!IsMaterialValid){
-            return;
-        }
-        m_OutlineRenderPass = new OutlineRenderPass(m_OutlineMaterial);
-
-    }
-
-    // Here you can inject one or multiple render passes in the renderer.
-    // This method is called when setting up the renderer once per-camera.
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-    {
-        if(m_OutlineRenderPass == null){
-            return;
-        }
-        renderer.EnqueuePass(m_OutlineRenderPass);
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-        m_OutlineRenderPass?.Dispose();
     }
 }
-
-
