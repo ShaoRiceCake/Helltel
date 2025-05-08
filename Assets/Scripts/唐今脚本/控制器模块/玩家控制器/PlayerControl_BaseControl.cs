61e5b1296f8ce68d9a20664683df2e91af9d2b86
TJA688 <jinjintangtang2@gmail.com>
2025-05-08 15:37:53 +0100

    导入一些粒子特效做实验

--- Assets/Scripts/唐今脚本/控制器模块/玩家控制器/PlayerControl_BaseControl.cs
+++ Assets/Scripts/唐今脚本/控制器模块/玩家控制器/PlayerControl_BaseControl.cs
@@ -6,6 +6,8 @@ public class PlayerControl_BaseControl : MonoBehaviour
     public PlayerControlInformationProcess controlHandler;
     public GameObject forwardObject;
 
+    public ParticleSystem controlParticleEffect; // 拖拽粒子特效预制体到这个变量
+    protected bool shouldShowParticles = false;
 
     protected virtual void Start()
     {
